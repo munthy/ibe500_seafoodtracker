7fa55df79fdfd303935a2800c7d577fc5931d81a
Vargrym <vengvall@gmail.com>
2022-04-21 13:04:05 +0200

    added dataanalysis export

--- src/libraries/ibe500sitefunctions.js
+++ src/libraries/ibe500sitefunctions.js
@@ -29,3 +29,102 @@ export async function getLotData(lotIdAsString){
     })
     return response.json();
 }
+
+export class DataAnalysis{
+    static ReturnCompanyStatsForLotNo(txDataArray){
+        let lotSummary = {};	
+        for (let txData of txDataArray){
+            initializeCatchInfo(txData);
+            initializeBuyerInfo(txData);
+            initializeSellerInfo(txData);
+            addSalesInfo(txData);
+            addPurchasedFromInfo(txData);
+            addSoldToInfo(txData);
+        }
+
+        return lotSummary;
+    
+        function initializeCatchInfo(tx){
+			if (tx.txType != "catch") {return;}
+			lotSummary.product = tx.product;
+			lotSummary.lotId = tx.lotId;
+			if (lotSummary[tx.buyer] == undefined){
+				lotSummary[tx.buyer] = {};				
+			}
+			lotSummary[tx.buyer].purchased = tx.quantity; //Choice made to represent catch as "buying" fish from the sea; easier to parse.            
+        }
+    
+        function initializeBuyerInfo(tx){
+            if (tx.txType != "purchase"){return;}
+			if(lotSummary[tx.buyer] == undefined){
+                lotSummary[tx.buyer] = {};
+                lotSummary[tx.buyer].purchased = 0;
+            }
+        }
+    
+        function initializeSellerInfo(tx){
+            if (tx.txType != "sale"){return;}
+			if(lotSummary[tx.seller] == undefined){
+                lotSummary[tx.seller] = {};
+                lotSummary[tx.seller].sold = 0;
+            }
+        }
+    
+        function addSalesInfo(tx){
+            if (tx.txType == "purchase"){
+                if (lotSummary[tx.buyer].purchased == undefined){lotSummary[tx.buyer].purchased = 0}
+                lotSummary[tx.buyer].purchased += tx.quantity;
+            }            
+
+            if (tx.txType == "sale"){
+                if (lotSummary[tx.seller].sold == undefined){lotSummary[tx.seller].sold = 0}
+                lotSummary[tx.seller].sold += tx.quantity;
+            }
+        }
+    
+        function addSoldToInfo(tx){
+            if (tx.txType != "sale"){return;}
+            if (lotSummary[tx.seller].soldTo == undefined) {lotSummary[tx.seller].soldTo = {}};
+            
+            lotSummary[tx.seller].soldTo[tx.buyer] = {};
+            lotSummary[tx.seller].soldTo[tx.buyer] = {
+                "product":tx.product,
+                "quantity":tx.quantity
+            };
+        }
+
+        function addPurchasedFromInfo(tx){
+            if (tx.txType != "purchase"){return;}
+            if (lotSummary[tx.buyer].purchasedFrom == undefined) {lotSummary[tx.buyer].purchasedFrom = {}};
+            
+            lotSummary[tx.buyer].purchasedFrom[tx.seller] = {};
+            lotSummary[tx.buyer].purchasedFrom[tx.seller] = {
+                "product":tx.product,
+                "quantity":tx.quantity
+            };
+        }
+    }
+
+	static CompareQuantity(lotSummary, seller, purchaser){
+		try{
+			if (lotSummary[seller].soldTo[purchaser].quantity == lotSummary[purchaser].purchasedFrom[seller].quantity){
+				return true;
+			}
+			return false;
+		}catch(error){
+			return false;
+		}
+	}
+	
+	static CompareProduct(lotSummary, seller, purchaser){
+		try{
+			if (seller == undefined || purchaser == undefined){return false;}
+			if(lotSummary[seller].soldTo[purchaser].product == lotSummary[purchaser].purchasedFrom[seller].product){
+				return true;
+			}
+			return false;
+		}catch(error){
+			return false;
+		}		
+	}
+}
